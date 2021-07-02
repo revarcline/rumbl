5ebee60d64fcbbd2ce9a2e06e3e4a37de7450edd
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-02 17:39:06 -0500

    controller time

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -9,12 +9,12 @@ defmodule Rumbl.Accounts do
     [
       %User{id: "1", name: "José", username: "josevalim"},
       %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"},
+      %User{id: "3", name: "Chris", username: "chrismccord"}
     ]
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map => map.id == id end)
+    Enum.find(list_users(), fn map -> map.id == id end)
   end
 
   def get_user_by(params) do
