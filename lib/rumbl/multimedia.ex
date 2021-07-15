cee75abdc6f7a5d4902e53f14e5cbd03abe16532
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-15 15:17:37 -0500

    it was in multimedia!

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -1,91 +1,52 @@
 defmodule Rumbl.Multimedia do
-  @moduledoc """
-  The Multimedia context.
-  """
-
   import Ecto.Query, warn: false
-  alias Rumbl.Repo
 
-  alias Rumbl.Multimedia.Video
-  alias Rumbl.Multimedia.Category
+  alias Rumbl.Repo
+  alias Rumbl.Multimedia.{Video, Category}
   alias Rumbl.Accounts
 
-  @doc """
-  Returns the list of videos.
-
-  ## Examples
-
-      iex> list_videos()
-      [%Video{}, ...]
-
-  """
   def list_videos do
     Repo.all(Video)
   end
 
-  @doc """
-  Returns the list of videos for a specific user.
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
 
-  ## Examples
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
 
-      iex> list_user_videos(user)
-      [%Video{}, ...]
-
-  """
   def list_user_videos(%Accounts.User{} = user) do
     Video
     |> user_videos_query(user)
-    |> Repo.all(Video)
+    |> Repo.all()
   end
 
-  @doc """
-  Gets a single video.
-
-  Raises `Ecto.NoResultsError` if the Video does not exist.
-
-  ## Examples
-
-      iex> get_video!(123)
-      %Video{}
-
-      iex> get_video!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_video!(id), do: Repo.get!(Video, id)
-
-  @doc """
-  Gets a single video from a given user.
-
-  Raises `Ecto.NoResultsError` if the Video does not exist for user.
-
-  ## Examples
-
-      iex> get_user_video!(user, 123)
-      %Video{}
-
-      iex> get_video!(user, 456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_user_video!(%Accounts.User{} = user, id) do
     Video
     |> user_videos_query(user)
-    |> Repo.get!(Video, id)
+    |> Repo.get!(id)
   end
 
-  @doc """
-  Creates a video.
+  def get_video!(id), do: Repo.get!(Video, id)
 
-  ## Examples
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
 
-      iex> create_video(user, %{field: value})
-      {:ok, %Video{}}
+  def update_video(%Video{} = video, attrs) do
+    video
+    |> Video.changeset(attrs)
+    |> Repo.update()
+  end
 
-      iex> create_video(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
 
-  """
   def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
@@ -93,85 +54,7 @@ defmodule Rumbl.Multimedia do
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a video.
-
-  ## Examples
-
-      iex> update_video(video, %{field: new_value})
-      {:ok, %Video{}}
-
-      iex> update_video(video, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_video(%Video{} = video, attrs) do
-    video
-    |> Video.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a video.
-
-  ## Examples
-
-      iex> delete_video(video)
-      {:ok, %Video{}}
-
-      iex> delete_video(video)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_video(%Video{} = video) do
-    Repo.delete(video)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking video changes.
-
-  ## Examples
-
-      iex> change_video(video)
-      %Ecto.Changeset{data: %Video{}}
-
-  """
-  def change_video(%Video{} = video, attrs \\ %{}) do
-    Video.changeset(video, attrs)
-  end
-
-  @doc """
-  Creates a category. Non-unique queries will return nil id.
-
-  ## Examples
-
-      iex> create_category(name)
-      %Rumbl.Multimedia.Category{}
-
-      iex> create_category(taken_name)
-      %Rumbl.Multimedia.Category{id: nil}
-
-  """
-  def create_category!(name) do
-    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
-  end
-
-  @doc """
-  Lists all categories alphabetically.
-
-  ## Examples
-
-      iex> list_alphabetical_categories()
-      [%Category{}, ...]
-
-  """
-  def list_alphabetical_categories do
-    Category
-    |> Category.alphabetical()
-    |> Repo.all()
-  end
-
-  defp user_videos_query(query, %Accounts.User{id: user_id}) do
-    from(v in query, where: v.user_id == ^user_id)
+  def change_video(%Video{} = video) do
+    Video.changeset(video, %{})
   end
 end
