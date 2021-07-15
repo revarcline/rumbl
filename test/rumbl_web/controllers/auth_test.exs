ead6b3398c932404ae40651c482029a9324d6292
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-15 16:23:00 -0500

    calling auth tests

--- test/rumbl_web/controllers/auth_test.exs
+++ test/rumbl_web/controllers/auth_test.exs
@@ -42,4 +42,20 @@ defmodule RumblWeb.AuthTest do
       |> Auth.logout()
       |> send_resp(:ok, "")
   end
+
+  test "call plases user from session into assigns", %{conn: conn} do
+    user = user_fixture()
+
+    conn =
+      conn
+      |> put_session(:user_id, user.id)
+      |> Auth.call(Auth.init([]))
+
+    assert conn.assigns.current_user.id == user.id
+  end
+
+  test "call with no session sets current_user assign to nil", %{conn: conn} do
+    conn = Auth.call(conn, Auth.init([]))
+    assert conn.assigns.current_user == nil
+  end
 end
