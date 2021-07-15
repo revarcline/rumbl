66991ae11468589b5807bf6b55eded825cd15651
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-15 16:24:08 -0500

    performance tweak on testing

--- config/test.exs
+++ config/test.exs
@@ -20,3 +20,6 @@ config :rumbl, RumblWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# speed up tests by hashing less
+config :pbkdf2_elixir, :rounds, 1
