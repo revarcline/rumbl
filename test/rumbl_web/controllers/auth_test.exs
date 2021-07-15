dac659ae85d6413ca22b4ef9753da4ad2662a6b8
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-15 15:39:09 -0500

    auth test working with conn bypass

---
+++ test/rumbl_web/controllers/auth_test.exs
@@ -0,0 +1,27 @@
+defmodule RumblWeb.AuthTest do
+  use RumblWeb.ConnCase, async: true
+  alias RumblWeb.Auth
+
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> bypass_through(RumblWeb.Router, :browser)
+      |> get("/")
+
+    {:ok, %{conn: conn}}
+  end
+
+  test "authenticate_user halts when no current_user exists", %{conn: conn} do
+    conn = Auth.authenticate_user(conn, [])
+    assert conn.halted
+  end
+
+  test "authenticate_user for existing current_user", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:current_user, %Rumbl.Accounts.User{})
+      |> Auth.authenticate_user([])
+
+    refute conn.halted
+  end
+end
