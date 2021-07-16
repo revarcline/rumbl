57807acd5e040977f5703c858ca928a7463d2685
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-16 11:46:20 -0500

    video js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.scss";
+import "../css/video.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,4 +13,12 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
+import "phoenix_html";
+
+import Player from "./player";
+let video = document.getElementById("video");
+if (video) {
+  Player.init(video.id, video.getAtttribute("data-player-id"), () => {
+    console.log("player ready!");
+  });
+}
