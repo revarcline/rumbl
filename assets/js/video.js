b45f369aa426cbabfaf659cc80ce914f32af53f9
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-17 14:39:50 -0500

    annotation frontend code

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,12 +1,12 @@
 import Player from "./player";
 
-let Video = {
+const Video = {
   init(socket, element) {
     if (!element) {
       return;
     }
-    let playerId = element.getAttribute("data-player-id");
-    let videoId = element.getAttribute("data-id");
+    const playerId = element.getAttribute("data-player-id");
+    const videoId = element.getAttribute("data-id");
     socket.connect();
     Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket);
@@ -14,12 +14,22 @@ let Video = {
   },
 
   onReady(videoId, socket) {
-    let msgContainer = document.getElementById("msg-container");
-    let msgInput = document.getElementById("msg-input");
-    let postButton = document.getElementById("msg-submit");
-    let vidChannel = socket.channel(`videos:${videoId}`);
-    vidChannel.on("ping", ({ count }) => console.log("PING", count));
+    const msgContainer = document.getElementById("msg-container");
+    const msgInput = document.getElementById("msg-input");
+    const postButton = document.getElementById("msg-submit");
+    const vidChannel = socket.channel(`videos:${videoId}`);
 
+    postButton.addEventListener("click", (e) => {
+      const payload = { body: msgInput.value, at: Player.getCurrentTime() };
+      vidChannel
+        .push("new_annotation", payload)
+        .receive("error", (e) => console.log(e));
+      msgInput.value = "";
+    });
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp);
+    });
     vidChannel
       .join()
       .receive("ok", (resp) => console.log("joined the video channel", resp))
