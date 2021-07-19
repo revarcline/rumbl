c6cfc066bbdb91a8adccf488f987dc20adaa7043
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-19 11:40:16 -0500

    resolved missing presence logic

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Accounts do
 
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
+  import Ecto.Query
 
   def get_user(id) do
     Repo.get(User, id)
@@ -20,6 +21,10 @@ defmodule Rumbl.Accounts do
 
   def list_users do
     Repo.all(User)
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from(u in User, where: u.id in ^ids))
   end
 
   def change_user(%User{} = user) do
