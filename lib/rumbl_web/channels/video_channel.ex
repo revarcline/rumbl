810f17a74219737854884970ce50ea36bcc7c215
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-18 20:10:21 -0500

    gotta figure out annotations

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -4,7 +4,15 @@ defmodule RumblWeb.VideoChannel do
   alias Rumbl.Multimedia
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    video_id = String.to_integer(video_id)
+    video = Multimedia.get_video!(video_id)
+
+    annotations =
+      video
+      |> Multimedia.list_annotations()
+      |> Phoenix.View.render_many(AnnotationView, "annotation.json")
+
+    {:ok, %{annotations: annotations}, assign(socket, :video_id, video_id)}
   end
 
   def handle_in(event, params, socket) do
