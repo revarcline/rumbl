09222882dd929f3c1fcef8f32f3a631182eea4ca
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-19 11:44:07 -0500

    usernames in js

--- assets/js/video.js
+++ assets/js/video.js
@@ -26,11 +26,10 @@ const Video = {
     const presence = new Presence(vidChannel);
 
     presence.onSync(() => {
-      console.log("presence synced");
       userList.innerHTML = presence
-        .list((id, { metas: [first, ...rest] }) => {
+        .list((id, { user: user, metas: [first, ...rest] }) => {
           let count = rest.length + 1;
-          return `<li>${id}: (${count})</li>`;
+          return `<li>${user.username}: (${count})</li>`;
         })
         .join("");
     });
